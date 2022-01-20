31a4b97cff472ffa47d5632ed92c0dea1fc5285a
Brendan Goodenough <brendan@goodenough.nz>
2022-01-20 20:47:29 +0100

    Add tests for ShopifyClient.queryWithPagination()

--- src/shopify/client.test.ts
+++ src/shopify/client.test.ts
@@ -1,9 +1,45 @@
 import test from 'ava'
-import { gql } from 'graphql-request'
+import sinon from 'sinon'
+import { GraphQLClient } from 'graphql-request'
+import { gql } from 'graphql-tag'
+import { ShopifyClient } from './client'
 
-test('queryWithPagination() queries all pages', async () => {
+test.beforeEach(() => {
+  sinon
+    .stub(GraphQLClient.prototype, 'request')
+    .onFirstCall()
+    .resolves({
+      products: {
+        pageInfo: {
+          hasNextPage: true,
+        },
+        edges: [
+          {
+            cursor: 'ABC=',
+          },
+          { cursor: 'DEF=' },
+        ],
+      },
+    })
+    .onSecondCall()
+    .resolves({
+      products: {
+        pageInfo: {
+          hasNextPage: false,
+        },
+        edges: [
+          {
+            cursor: 'GHI=',
+          },
+          { cursor: 'JKL=' },
+        ],
+      },
+    })
+})
+
+test('queryWithPagination() queries all pages', async (t) => {
   const query = gql`
-    query ($first: Int!, after: String) {
+    query ($first: Int!, $after: String) {
       products(first: $first, after: $after) {
         edges {
           cursor
@@ -13,7 +49,14 @@ test('queryWithPagination() queries all pages', async () => {
         }
       }
     }
-
-
   `
+
+  const client = new ShopifyClient()
+  const edges = await client.queryWithPagination(query, 10)
+
+  t.assert(edges.length === 4, 'response has 4 edges')
+  t.deepEqual(
+    edges.map(({ cursor }) => cursor),
+    ['ABC=', 'DEF=', 'GHI=', 'JKL=']
+  )
 })
